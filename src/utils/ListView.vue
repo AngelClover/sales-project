22ebb46ba913f7bbed0c5e2da0cbe5c51ef01db9
changhaozhe <changhaozhe@baidu.com>
2017-01-31 08:36:47 +0800

    add filter && sort && little decorating

--- src/utils/ListView.vue
+++ src/utils/ListView.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
+        <div>
+            <form id="search">
+                search : <input name="query" v-model="searchQuery">
+            </form>
+            <br/>
+        </div>
         <table>
             <thead>
                 <tr>
                     <!--
             <th v-for="(val, key, index) in title"> {{val}} </th>
                     -->
-            <th v-for="(item, index) in preference"> {{title[item]}} </th>
+            <th v-for="(item, index) in preference"
+                @click="sortBy(item)"
+                :class="{ active : sortKey == item}" >
+                {{title[item] | capitalize}}
+                <span class="arrow" :class="sortOrders[item] > 0 ? 'asc' : 'dsc'">
+                </span>
+            </th>
                 </tr>
             </thead>
             <tbody>
-            <tr v-for="(item, index) in content"> 
+            <tr v-for="(item, index) in filteredContent"> 
                 <!--
                 <td v-for="(val, key, index) in title">{{item[key]}}</td>
                 -->
@@ -29,6 +41,10 @@
         <p> content : {{content}} </p>
         <p> pref(pass in from props) : {{pref}} </p>
         <p> preference(final effects) : {{preference}} </p>
+        <p> sortOrders : {{sortOrders}} </p>
+        <p> sortKey : {{sortKey}} </p>
+        <p> searchQuery : {{searchQuery}} </p>
+        <p> filteredContent : {{filteredContent}} </p>
 
     </div>
 </template>
@@ -53,6 +69,13 @@ export default {
         }
     },
     */
+    data : function(){
+        return {
+            sortKey : '',
+            sortOrders : {},
+            searchQuery : '',
+        }
+    },
     computed : {
         preference : function(){
             console.log('preference in listview refresh', this)
@@ -73,11 +96,114 @@ export default {
             return {
                 ...ret
             }
-        }
-    }
+        },
+        filteredContent : function() {
+            //console.log('filteredContent computed')
+            var sortKey = this.sortKey
+            var filterKey = this.filterKey && this.filterKey.toLowerCase()
+            if (this.sortOrders[sortKey] == undefined){
+                this.initSortOrders()
+            }
+            var order = this.sortOrders[sortKey] || 1
+            var data = this.content
+            if (filterKey) {
+                data = data.filter(function (row) {
+                    return Object.keys(row).some(function (key) {
+                        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+                    })
+                })
+            }
+            //console.log('sortKey ', sortKey, 'sortorder', order)
+            if (sortKey) {
+                data = data.slice().sort(function (a, b) {
+                    a = a[sortKey]
+                    b = b[sortKey]
+                    return (a === b ? 0 : a > b ? 1 : -1) * order
+                })
+            }
+            //console.log('filteredContent computed end')
+            return data
+        },
+    },
+   filters: {
+       capitalize: function (str) {
+           return str.charAt(0).toUpperCase() + str.slice(1)
+       }
+   },
+   methods: {
+       sortBy: function (key) {
+           //console.log('sortBy', key)
+           this.sortKey = key
+           this.sortOrders[key] = this.sortOrders[key] * -1
+       },
+       initSortOrders : function(){
+            var sortOrders = {}
+            //console.log('listview method initSortOrders', Object.keys(this.title))
+            Object.keys(this.title).forEach(function (key) {
+                sortOrders[key] = 1
+            })
+            //console.log('listview method initSortOrders :', sortOrders, Object.keys(sortOrders))
+            //this.$set('sortOrders', sortOrders)
+            this.sortOrders = sortOrders
+            return sortOrders
+       }
+   }
 }
 </script>
 
 <style>
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255,255,255,0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+th.active {
+  color: #fff;
+}
+
+th.active .arrow {
+  opacity: 1;
+}
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid #fff;
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid #fff;
+}
 </style>
 
